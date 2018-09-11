469912b8cc451c3ebf47b13c3ce5ec3a8075139c
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-11 23:46:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
 	    
 	  }, 
 	  :rent_in_dollars => { 
-	    one_pieced_owned: 25,
+	    one_piece_owned: 25,
 	    two_pieced_owned: 50,
 	    three_pieced_owned: 100,
 	    four_pieced_owned: 200,
